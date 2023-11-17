f98b34b9704dbb4829401f582451e792037b678a
Ilya-Kovalenko <98124003+Ilya-Kovalenko@users.noreply.github.com>
2023-11-17 23:38:17 +0300

    feat: change migration
    the migration mechanism has been changed. now, during server startup, the current state of the database collection is compared with what is specified in the configuration file, and if they do not match, the migration is performed again

--- utils/db_worker.py
+++ utils/db_worker.py
@@ -1,7 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://root:example@mongo_db/?authMechanism=DEFAULT")
+from config import MONGODB_URL, FORMS
 
 
 async def find_form(form: dict) -> dict:
@@ -23,41 +21,15 @@ async def find_form(form: dict) -> dict:
 async def migrate() -> None:
     mongo_client = AsyncIOMotorClient(MONGODB_URL)["base"]
 
-    if await mongo_client.form_templates.find({}).to_list(length=100) != []:
+    db_forms = await mongo_client.form_templates.find({}).to_list(length=100)
+
+    for el in db_forms:
+        el.pop("_id")
+
+    if db_forms == FORMS:
         return
 
-    forms = [
-        {
-            "name": "form one",
-            "username": "text",
-            "password": "text",
-            "user_email": "email",
-            "user_phone": "phone",
-            "date_created": "date"
-        },
-        {
-            "name": "phone form",
-            "username": "text",
-            "mobile_phone": "phone",
-            "home_phone": "phone",
-            "work_phone": "phone",
-            "date_created": "date"
-        },
-        {
-            "name": "Form template name",
-            "name_field": "text",
-            "email_field": "email",
-            "phone_field": "phone",
-            "date_field": "date"
-        },
-        {
-            "name": "Text form",
-            "text1": "text",
-            "text2": "text",
-            "text3": "text",
-            "date_created": "date"
-        }
-    ]
+    await mongo_client.form_templates.delete_many({})
 
-    for form in forms:
+    for form in FORMS:
         await mongo_client.form_templates.insert_one(form)
